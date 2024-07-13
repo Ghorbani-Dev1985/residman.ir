1ee100caab08004d0ead4379537e5eec2c01c4a9
ghorbani-dev1985 <ghorbani.dev1985@gmail.com>
2024-07-13 11:37:33 +0330

    fix: error of setSortByDate type error

--- components/SortPostLink.tsx
+++ components/SortPostLink.tsx
@@ -1,12 +1,13 @@
 import { ArrowsUpDownIcon , ArrowTopRightOnSquareIcon} from "@heroicons/react/24/outline";
 import Link from "next/link";
+import { Dispatch, SetStateAction } from "react";
 type SortProp = {
-    setSortByDate : boolean
+    setSortByDate : Dispatch<SetStateAction<boolean>>
 }
 const SortPostLink: React.FC<SortProp> = ({setSortByDate}) => {
     return(
         <div className="w-full flex justify-between items-center my-6">
-        <p onClick={() => setSortByDate((prev : boolean) => !prev)} className="flex justify-center items-center gap-x-1.5 cursor-pointer text-zinc-400 hover:text-zinc-800 transition-colors select-none"><ArrowsUpDownIcon className="size-5"/>مرتب سازی تاریخ</p>
+        <p onClick={() => setSortByDate((prev) => !prev)} className="flex justify-center items-center gap-x-1.5 cursor-pointer text-zinc-400 hover:text-zinc-800 transition-colors select-none"><ArrowsUpDownIcon className="size-5"/>مرتب سازی تاریخ</p>
         <Link target="_blank" href="http://tracking.post.ir/" className="flex justify-center items-center gap-x-1.5 bg-sky-100 text-sky-500 font-bold hover:bg-white px-1.5 md:px-5 py-2 rounded-lg transition-colors">
         <ArrowTopRightOnSquareIcon className="size-5 -rotate-90"/>
        <span className="select-none text-sm md:text-base">پیگیری سفارش</span> </Link>
